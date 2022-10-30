58ee943ecfe5858d50370d9d2b9376573565094a
Manmohan Singh Kataria <themanmohankataria@gmail.com>
2022-10-30 22:21:18 +0530

    created Component  Section

--- Components/HeaderTabs.js
+++ Components/HeaderTabs.js
@@ -26,13 +26,13 @@ export default function HeaderTabs() {
 const HeaderButton = (props) => (
 
 <TouchableOpacity style={
-    {backgroundColor: props.btnColor,
+    {backgroundColor: props.activeTab === props.text ? "black" : "white",
     paddingVertical: 6,
     paddingHorizontal: 16,
     borderRadius: 30,
 }}
 onPress={()=>props.setActiveTab(props.text)}
 >
-<Text style={{color: props.textColor, fontSize: 15, fontWeight:"900"}}>{props.text}</Text>
+<Text style={{color: props.activeTab === props.text ? "white" : "black ", fontSize: 15, fontWeight:"900"}}>{props.text}</Text>
 </TouchableOpacity>
 );
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,11 +1,17 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import HeaderTabs from '../Components/HeaderTabs'
+import SearchBar from '../Components/SearchBar'
+import Categories from '../Components/Categories'
 
 export default function Home() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1}}>
+    <View style={{ backgroundColor:"#fff", padding: 15}}>
      <HeaderTabs />
+     <SearchBar />
+    </View>
+    <Categories />
     </SafeAreaView>
   )
 }
